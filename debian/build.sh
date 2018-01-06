2d0f7d54fb19ac235e85f5843c56c00961135112
SR_team <sr-tream@yandex.ru>
2018-01-06 17:20:10 +0300

    fix script 'build.sh'

--- debian/build.sh
+++ debian/build.sh
@@ -3,5 +3,5 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 sudo docker build -t winebottle_builder $SCRIPTPATH
-mkdir build build/bin
+mkdir $SCRIPTPATH/build $SCRIPTPATH/build/bin
 sudo docker run -v $SCRIPTPATH/../:/root/WineBottle winebottle_builder
